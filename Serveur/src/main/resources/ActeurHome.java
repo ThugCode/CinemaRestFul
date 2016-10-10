f03969adde9664549257d0f4755a7f720145e533
Letourneur Léo <letourneur.leo@gmail.com>
2016-10-10 22:35:09 +0200

    Lien hibernate/java

---
+++ Serveur/src/main/resources/ActeurHome.java
@@ -0,0 +1,118 @@
+package main.resources;
+
+// default package
+// Generated 10 oct. 2016 11:51:28 by Hibernate Tools 3.4.0.CR1
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Acteur.
+ * @see .Acteur
+ * @author Hibernate Tools
+ */
+public class ActeurHome {
+
+	private static final Log log = LogFactory.getLog(ActeurHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext().lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Acteur transientInstance) {
+		log.debug("persisting Acteur instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Acteur instance) {
+		log.debug("attaching dirty Acteur instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Acteur instance) {
+		log.debug("attaching clean Acteur instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Acteur persistentInstance) {
+		log.debug("deleting Acteur instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Acteur merge(Acteur detachedInstance) {
+		log.debug("merging Acteur instance");
+		try {
+			Acteur result = (Acteur) sessionFactory.getCurrentSession().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Acteur findById(int id) {
+		log.debug("getting Acteur instance with id: " + id);
+		try {
+			Acteur instance = (Acteur) sessionFactory.getCurrentSession().get("Acteur", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Acteur instance) {
+		log.debug("finding Acteur instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("Acteur").add(Example.create(instance))
+					.list();
+			log.debug("find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
---
+++ Serveur/src/main/resources/CategorieHome.java
@@ -0,0 +1,116 @@
+// default package
+// Generated 10 oct. 2016 11:51:28 by Hibernate Tools 3.4.0.CR1
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Categorie.
+ * @see .Categorie
+ * @author Hibernate Tools
+ */
+public class CategorieHome {
+
+	private static final Log log = LogFactory.getLog(CategorieHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext().lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Categorie transientInstance) {
+		log.debug("persisting Categorie instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Categorie instance) {
+		log.debug("attaching dirty Categorie instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Categorie instance) {
+		log.debug("attaching clean Categorie instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Categorie persistentInstance) {
+		log.debug("deleting Categorie instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Categorie merge(Categorie detachedInstance) {
+		log.debug("merging Categorie instance");
+		try {
+			Categorie result = (Categorie) sessionFactory.getCurrentSession().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Categorie findById(java.lang.String id) {
+		log.debug("getting Categorie instance with id: " + id);
+		try {
+			Categorie instance = (Categorie) sessionFactory.getCurrentSession().get("Categorie", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Categorie instance) {
+		log.debug("finding Categorie instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("Categorie").add(Example.create(instance))
+					.list();
+			log.debug("find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
