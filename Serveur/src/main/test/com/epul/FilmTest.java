b3439fbd5f01a5df5c2ae794f40f3bf2fd6e88be
Letourneur Léo <letourneur.leo@gmail.com>
2016-10-29 10:59:31 +0200

    Test unitaires

--- Serveur/src/main/test/com/epul/FilmTest.java
+++ Serveur/src/main/test/com/epul/FilmTest.java
@@ -40,104 +40,138 @@ public class FilmTest {
 	@Test
 	public void testList() {
 		
-		try {
-			WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films");
-			ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+		System.out.println("");
+		System.out.println("SERVER REQUEST : LIST");
+		System.out.println("");
+		
+		WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films");
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
 
-			System.out.println("Server response : \n"+response.getEntity(String.class));
-			
-			assertTrue(response.getStatus() == 200);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println("Server response : \n"+response.getEntity(String.class));
+		
+		assertTrue(response.getStatus() == 200);
 	}
 	
 	@Test
 	public void testAdd() {
 		
-		try {
-
-			WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/add/");
-			ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, filmToTest);
-			
-			System.out.println("Server response : \n"+response.getEntity(String.class));
-			
-			assertTrue(response.getStatus() == 200);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println("");
+		System.out.println("SERVER REQUEST : ADD");
+		System.out.println("");
+		
+		WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/add/");
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, filmToTest);
+		
+		System.out.println("Server response : \n"+response.getEntity(String.class));
+		
+		assertTrue(response.getStatus() == 200);
+	}
+	
+	@Test
+	public void testAddDuplicateEntry() {
+		
+		System.out.println("");
+		System.out.println("SERVER REQUEST : ADD DUPLICATE");
+		System.out.println("");
+		
+		WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/add/");
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, filmToTest);
+		
+		assertTrue(response.getEntity(String.class).contains("Duplicate entry"));
 	}
 	
 	@Test
 	public void testUpdate() {
 		
+		System.out.println("");
+		System.out.println("SERVER REQUEST : UPDATE");
+		System.out.println("");
+		
 		filmToTest.setTitre("Patachou");
 		
-		try {
+		WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/update/");
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, filmToTest);
+		
+		System.out.println("Server response : \n"+response.getEntity(String.class));
 
-			WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/update/");
-			ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, filmToTest);
-			
-			System.out.println("Server response : \n"+response.getEntity(String.class));
+		assertTrue(response.getStatus() == 200);
+	}
+	
+	@Test
+	public void testUpdateFalse() {
+		
+		System.out.println("");
+		System.out.println("SERVER REQUEST : UPDATE FALSE");
+		System.out.println("");
+		
+		filmToTest.setNoFilm(13);
+		filmToTest.setTitre("Patachou");
+		
+		WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/update/");
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, filmToTest);
+		
+		System.out.println("Server response : \n"+response.getEntity(String.class));
 
-			assertTrue(response.getStatus() == 200);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		assertTrue(response.getStatus() == 200);
 	}
 	
 	@Test
 	public void testGet() {
 		
-		try {
+		System.out.println("");
+		System.out.println("SERVER REQUEST : GET");
+		System.out.println("");
+		
+		WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/get/"+filmToTest.getNoFilm());
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+		
+		System.out.println("Server response : \n"+response.getEntity(String.class));
 
-			WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/get/"+filmToTest.getNoFilm());
-			ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-			
-			System.out.println("Server response : \n"+response.getEntity(String.class));
+		assertTrue(response.getStatus() == 200);
+	}
+	
+	@Test
+	public void testGetFalse() {
+		
+		System.out.println("");
+		System.out.println("SERVER REQUEST : GET FALSE");
+		System.out.println("");
+		
+		WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/get/14");
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+		
+		System.out.println("Server response : \n"+response.getEntity(String.class));
 
-			assertTrue(response.getStatus() == 200);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		assertTrue(response.getStatus() == 200);
 	}
 	
 	@Test
 	public void testSearch() {
 		
-		try {
+		System.out.println("");
+		System.out.println("SERVER REQUEST : SEARCH");
+		System.out.println("");
+		
+		WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/search/");
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, filmToTest);
+		
+		System.out.println("Server response : \n"+response.getEntity(String.class));
 
-			WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/search/");
-			ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, filmToTest);
-			
-			System.out.println("Server response : \n"+response.getEntity(String.class));
-
-			assertTrue(response.getStatus() == 200);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		assertTrue(response.getStatus() == 200);
 	}
 	
 	@Test
 	public void testDelete() {
 		
-		try {
-			
-			WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/delete/"+filmToTest.getNoFilm());
-			ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-			
-			System.out.println("Server response : \n"+response.getEntity(String.class));
+		System.out.println("");
+		System.out.println("SERVER REQUEST : DELETE");
+		System.out.println("");
+		
+		WebResource webResource = client.resource("http://localhost:8080/CinemaRestFulServeur/test/films/delete/"+filmToTest.getNoFilm());
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+		
+		System.out.println("Server response : \n"+response.getEntity(String.class));
 
-			assertTrue(response.getStatus() == 200);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		assertTrue(response.getStatus() == 200);
 	}
-
 }
